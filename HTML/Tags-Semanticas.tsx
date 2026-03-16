2bbce54aa0de7d4774092450b5bd4180b8035ac6
MAlmeida3734 <macunhadealmeida@gmail.com>
2026-03-16 15:50:51 -0300

    Create first commit

--- HTML/Tags-Semanticas.tsx
+++ HTML/Tags-Semanticas.tsx
@@ -12,5 +12,61 @@
 
 // {/* <b></b> --> em negrito
 // <strong></strong> --> mudar a palavra de forma semântica 
-// <i></i> --> deixar a palavra em itálico
-// <em></em> --> dar enfãse nas palavras */}
+//  <i></i> --> deixar a palavra em itálico
+// <em></em> --> dar enfãse nas palavras
+
+// #########################################################################
+// <style>
+//         .img-in-text {
+//             width: 50px;
+//             height: 44px;
+
+//         }
+
+//         .marcação {
+//             color: aqua;
+//             font-size: 30px;
+//         }
+//     </style>
+// </head>
+
+// <body>
+//     <h1> Cabeçalho</h1>
+//     <h1> Cabeçalho</h1>
+//     <p>
+//         <b>Lorem</b> <strong>dolor</strong> <i>sit</i> <em>amet</em> consectetur adipisicing elit.
+
+//         <a href="https://validator.w3.org/" target="_blank">Itaque</a>
+
+//         <a href="https://validator.w3.org/" target="_blank"><img class="img-in-text"
+//                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fassets.stickpng.com%2Fimages%2F58480eb3cef1014c0b5e492a.png&f=1&nofb=1&ipt=0105b0df9f4be927857e0c09dd85032dff6def2d34d8c99f0e6cc044ebe62db2"
+//                 alt="Logo da MDN"></a>
+
+//         <del>vermelho</del> <ins>azul</ins>, <s>iste</s> <u>veniam</u> <small>in</small> porro placeat 2<sup>2</sup>
+//         H<sub>2</sub>O perspiciatis
+//         et magni tempore odit vero, <br>aliquid <span class="marcação">dolore</span> beatae aspernatur,
+//         inventore debitis minus
+
+//         <q cite="http://127.0.0.1:5500/HTML/index.html">Lorem dolor</q>.
+//         Animi.
+//     </p>
+
+
+//     <hr>
+
+//     <pre><code>let soma = 0;
+// inputsMais.forEach((input) => {
+// const valor = input.value;
+// const numero = Number(valor);
+// if (numero) {
+// soma += numero;
+// }
+// console.log(soma)
+// })</code>
+//     </pre>
+
+//     <blockquote cite="http://127.0.0.1:5500/HTML/index.html">
+//         Agora estou fazendo uma citção!
+//     </blockquote>
+//     <div class="marcação">Eu sou um bloco</div>
+// </body>
