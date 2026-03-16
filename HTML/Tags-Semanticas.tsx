e4b76cfcca01329e4096b59f8e004466d0a3a052
MAlmeida3734 <macunhadealmeida@gmail.com>
2026-03-16 15:42:38 -0300

    Create first commit

---
+++ HTML/Tags-Semanticas.tsx
@@ -0,0 +1,16 @@
+// {/* <body>
+//     <h1> Cabeçalho</h1>
+//     <h1> Cabeçalho</h1>
+//     <p>
+//         <b>Lorem</b> <strong>dolor</strong> <i>sit</i> <em>amet</em> consectetur adipisicing elit.
+//         Itaque, iste veniam in porro placeat dicta perspiciatis
+//         et magni tempore odit vero, aliquid dolore beatae aspernatur,
+//         inventore debitis minus laboriosam.
+//         Animi.
+//     </p>
+// </body> */}
+
+// {/* <b></b> --> em negrito
+// <strong></strong> --> mudar a palavra de forma semântica 
+// <i></i> --> deixar a palavra em itálico
+// <em></em> --> dar enfãse nas palavras */}
---
+++ HTML/cabecalhos.tsx
@@ -0,0 +1,31 @@
+//  <h1>Meu título</h1>
+//     <p>
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit.
+//         Natus repellat eaque consectetur ea excepturi.
+//         Excepturi, aliquid!
+//         Modi, nesciunt. Dicta vitae tenetur fugit distinctio sed, earum iste accusantium voluptatum
+//         repellat nemo.
+//     </p>
+//     <h2>Meu título</h2>
+//     <p>
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit.
+//         Natus repellat eaque consectetur ea excepturi.
+//         Excepturi, aliquid!
+//         Modi, nesciunt. Dicta vitae tenetur fugit distinctio sed, earum iste accusantium voluptatum
+//         repellat nemo.
+//     </p>
+//     <h3>Meu título</h3>
+//     <p>
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit.
+//         Natus repellat eaque consectetur ea excepturi.
+//         Excepturi, aliquid!
+//         Modi, nesciunt. Dicta vitae tenetur fugit distinctio sed, earum iste accusantium voluptatum
+//         repellat nemo.
+//     </p>
+//     <h4>Meu título</h4>
+
+//     <h5>Meu título</h5>
+
+//     <h6>Meu título</h6>
+
+// Dependendo do "h", gera a hierarquia : h1 > h2 > h3 > h4 > h5 > h6
