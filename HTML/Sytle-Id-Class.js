9c015290486adfb7b65a1bdc559f7d85e7969dec
MAlmeida3734 <macunhadealmeida@gmail.com>
2026-03-16 12:57:39 -0300

    Create first commit

---
+++ HTML/Sytle-Id-Class.js
@@ -0,0 +1,22 @@
+
+
+// {/* <style>
+//         h1 {
+//             background: red; 
+//         }
+//         p {
+//             background: blue;
+//         }
+
+//         #heading-um {
+//             background: purple;  #--> id
+//         }
+
+//         .fundo-vermelho {
+//             background: red;  . --> class
+//         }
+//         .bold{
+//             color: yellow;   . --> class
+//         }
+
+//     </style> */}
