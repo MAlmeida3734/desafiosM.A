45e6acc5de13cc2e148ad13559adf9ee0d154370
MAlmeida3734 <macunhadealmeida@gmail.com>
2026-04-24 23:54:59 -0300

    Create first commit

--- rastreador-MA/src/search.js
+++ rastreador-MA/src/search.js
@@ -1,13 +1,74 @@
 function rastrear() {
-      var codigo = document.getElementById('codigoRastreio').value;
-      if (codigo) {
-        window.open('https://www.linkcorreios.com.br/?id=' + codigo, '_blank');
-      } else {
-        alert('Por favor, digite o código de rastreio.');
-      }
+    const codigoInput = document.getElementById('codigoRastreio');
+    const codigo = codigoInput.value.trim().toUpperCase();
 
-      document.getElementById('codigoRastreio').addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-        rastrear();
+    if (codigo === "") {
+        if (typeof Swal !== 'undefined') {
+            Swal.fire({
+                title: 'Atenção!',
+                text: 'Digite um código de rastreio.',
+                icon: 'warning',
+                confirmButtonColor: '#00416b'
+            });
+        } else {
+            alert("Por favor, digite um código."); 
+        }
+        return;
     }
-})}
+
+     if (codigo.length !== 13) {
+        Swal.fire({
+            title: 'Código Inválido',
+            text: 'Um código padrão tem 13 caracteres (Ex: AA123456789BR).',
+            icon: 'warning',
+            confirmButtonColor: '#00416b'
+        });
+        return;
+    }
+    
+    
+    document.getElementById('codigoRastreio').addEventListener('keypress', function (e) {
+        if (e.key === 'Enter') {
+            rastrear();
+        }
+    })
+
+    salvarNoHistorico(codigo);
+    const url = `https://correios.com.br{codigo}`;
+    window.open(url, '_blank');
+    
+    codigoInput.value = "";
+    exibirHistorico();
+}
+
+function salvarNoHistorico(codigo) {
+    let historico = JSON.parse(localStorage.getItem('rastreios')) || [];
+    
+    if (!historico.includes(codigo)) {
+        historico.push(codigo);
+        localStorage.setItem('rastreios', JSON.stringify(historico));
+    }
+}
+
+function exibirHistorico() {
+    const lista = document.getElementById('listaHistorico');
+    let historico = JSON.parse(localStorage.getItem('rastreios')) || [];
+    
+    lista.innerHTML = historico.map(cod => `
+        <li>
+            <span>${cod}</span>
+            <button onclick="removerItem('${cod}')">x</button>
+        </li>
+    `).join('');
+}
+
+function removerItem(codigo) {
+    let historico = JSON.parse(localStorage.getItem('rastreios')) || [];
+    historico = historico.filter(item => item !== codigo);
+    localStorage.setItem('rastreios', JSON.stringify(historico));
+    exibirHistorico();
+}
+
+document.getElementById('codigoRastreio').addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') rastrear();
+});
