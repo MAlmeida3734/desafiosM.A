34da5aedc961cefe00b8c4487b90423cb2b41bc0
MAlmeida3734 <macunhadealmeida@gmail.com>
2026-04-26 17:58:56 -0300

    Create first commit

--- rastreador-MA/remove.js
+++ rastreador-MA/remove.js
@@ -1,8 +1,7 @@
 function removerItem(codigo) {
-    const getHistorico = () => JSON.parse(localStorage.getItem('rastreios')) || [];
-    const historico = getHistorico();
-    let hist = JSON.parse(localStorage.getItem('rastreios')) || [];
-    hist = historico.filter(item => item !== codigo);
+
+    let historico = JSON.parse(localStorage.getItem('rastreios')) || [];
+    historico = historico.filter(item => item !== codigo);
     localStorage.setItem('rastreios', JSON.stringify(historico));
     exibirHistorico();
 }
