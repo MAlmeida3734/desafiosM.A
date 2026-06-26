b9d20ad53785b55f70628687eb771b0cec75c4e2
MAlmeida3734 <macunhadealmeida@gmail.com>
2026-06-26 16:08:14 -0300

    First journey

--- my/src/app.vue
+++ my/src/app.vue
@@ -1,81 +1,11 @@
 <template>
     <div id="app">
         <MaComponents/>
-        
-        
-        <div>
-            
-            <label>NewsLetter</label> <br>
-            
-            <input
-            v-model="newsletter"
-            type="radio"
-            value="Sim"
-            > Sim
-            
-            <input
-            v-model="newsletter"
-            type="radio"
-            value="Não"
-            >Não <br>
-            {{ newsletter }}
-            
-        </div>
-    <div>
-        <button v-on:click="onClick()">
-            Enviar
-        </button>
-    </div>
-    <br>
-    <br>
-    <br>
-    <br>
-    
-    <form action="https://google.com"
-    @submit.prevent="onSubmit">
-    
-    <button type="submit">
-        Enviar
-    </button>
-    
-</form>
 <div>
     
     {{ fullName }}
 </div>
 
-<h2> Todos em aberto</h2>
-<div
-v-for="todo in uncompletedTodo"
-:key="todo.id"
->
-{{ todo.title }} 
-</div>
-
-<h2>Todos completas</h2>
-<div
-v-for="todo in completedTodo"
-:key="todo.id"
->
-{{ todo.title }}
-</div>
-
-<br><br><br>
-
-<h2>Todos</h2>
-<div
-v-for="todo in todos"
-:key="todo.id"
->
-
-<input 
-v-model="todo.completed"
-type="checkbox">
-
-
-
-{{ todo.title }}
-</div>
 
 <div>
     <img
@@ -103,41 +33,9 @@ export default {
             user: { fisrt_name: 'Marco',
             last_name:'Almeida',
             },
-            newsletter: '',
-            contract: false,
-            colors: [],
-            todos: [
-  {
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "quis ut nam facilis et officia qui",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 3,
-    "title": "fugiat veniam minus",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 4,
-    "title": "et porro tempora",
-    "completed": true
-  },
-  {
-    "userId": 1,
-    "id": 5,
-    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-    "completed": false
-  }]
         }},
+
+        watch: {},
         computed: {
             fullName(){
                 return `${this.user.fisrt_name} ${this.user.last_name}`
