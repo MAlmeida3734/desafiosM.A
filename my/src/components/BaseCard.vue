b3f164cd516a3e44d1f78e6d4f8c8e7b85f6584c
MAlmeida3734 <macunhadealmeida@gmail.com>
2026-07-09 01:28:00 -0300

    First journey

--- my/src/components/BaseCard.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="card">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur eligendi, nisi reiciendis modi at nesciunt minima, sint sequi esse autem quaerat placeat similique cum beatae animi ratione quibusdam obcaecati laborum.
-    </div>
-</template>
--- my/src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from "vue";
-import App from "./app.vue";
-import "./assets/css/styles.css"
-window.app = createApp(App).mount('#app' )
-
